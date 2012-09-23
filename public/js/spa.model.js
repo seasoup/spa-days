4ba750b0623956c519596e2328f0371e9f0ce10c
Michael Mikowski <mmikowski@snaplogic.com>
2012-09-23 13:30:18 -0700

    Fixed spa.model.js, wip restructure per architecture

---
+++ public/js/spa.model.js
@@ -0,0 +1,28 @@
+/*
+ *   spa.data.js
+ *   data module
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+spa.model = (function (){
+  var
+    stateMap = { },
+    initModule
+    ;
+
+
+  initModule = function (){};
+
+  return {
+    initModule : initModule
+  };
+}());
+
